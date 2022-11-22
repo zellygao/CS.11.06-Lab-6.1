49a2b183280e9cbf96a0b9a710d4c7d50b2fd959
Adam <adam.ewing.nz@gmail.com>
2022-11-22 10:50:19 +0800

    Initial commit

---
+++ src/AdventureTime.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class AdventureTime {
+
+    /** This is the main method and it is where you will test your implementations for challengeOne, challengeTwo, etc.
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+
+    }
+
+    /** TODO 1
+     *
+     * Challenge 1
+     *
+     * @param fileName
+     * @return Answer to Challenge 1
+     * @throws IOException
+     */
+    public static int challengeOne(String fileName) throws IOException {
+        return 0;
+    }
+
+    /** TODO 2
+     *
+     * Challenge 2
+     *
+     * @param fileName
+     * @return Answer to Challenge 2
+     * @throws FileNotFoundException
+     */
+    public static int challengeTwo(String fileName) throws FileNotFoundException {
+        return 0;
+    }
+
+    /** TODO 3
+     *
+     * Challenge 3
+     *
+     * @param fileName
+     * @return Answer to Challenge 3
+     * @throws FileNotFoundException
+     */
+    public static int challengeThree(String fileName) throws FileNotFoundException {
+        return 0;
+    }
+
+    /** TODO 4
+     *
+     * Challenge 4
+     *
+     * @param filename
+     * @return Answer to Challenge 4
+     * @throws FileNotFoundException
+     */
+    public static int challengeFour(String filename) throws FileNotFoundException {
+        return 0;
+    }
+
+    /** This method will write the values passed as challengeOne, challengeTwo, challengeThree, and challengeFour to a text file.
+     * Do not edit this method.
+     */
+    private static void writeFileAllAnswers(String outPutFilename, int challengeOne, int challengeTwo, int challengeThree, int challengeFour) throws IOException {
+        File file = new File(outPutFilename);
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+        bufferedWriter.write("Challenge 1: " + "\t" + challengeOne + "\n");
+        bufferedWriter.write("Challenge 2: " + "\t" + challengeTwo + "\n");
+        bufferedWriter.write("Challenge 3: " + "\t" + challengeThree + "\n");
+        bufferedWriter.write("Challenge 4: " + "\t" + challengeFour + "\n");
+        bufferedWriter.close();
+    }
+
+    /** This method will read the values in inputFilename into an array of integers, which it will return.
+     * Do not edit this method.
+     */
+    private static int[] readFile(String inputFilename, int numberOfElements) throws FileNotFoundException {
+        File file = new File(inputFilename);
+        Scanner scanner = new Scanner(file);
+        int[] data = new int[numberOfElements];
+        int index = 0;
+        while (scanner.hasNext()) {
+            data[index++] = scanner.nextInt();
+        }
+        scanner.close();
+        return data;
+    }
+
+}
